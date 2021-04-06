04b05934e3da55f85bf1abebbca11ce64fcd347f
JRicardoF <ricardosfj1@hotmail.com>
2021-04-06 08:54:36 -0700

    Add project files.

---
+++ PassCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PassCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Pass = "";
+            var CheckPass = "Secret";
+
+            while (Pass != CheckPass)
+            { 
+            Console.WriteLine("Please insert your Username");
+            var UserName = Console.ReadLine();
+
+            Console.WriteLine("Please insert your Password");
+            Pass = Console.ReadLine();            
+
+            if (Pass != CheckPass)
+                    { 
+                Console.WriteLine("UserName or Password does not Match. Please try Again");
+                    }
+            }
+
+            Console.WriteLine("You are now Logged in.");
+        }
+
+    }
+}
